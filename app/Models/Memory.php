532937ddce794e706ec0020d5cbdd1b31ac27a92
Murat Adıyaman <muratadiyaman0@gmail.com>
2026-08-06 18:00:58 +0300

    updated memory model

--- app/Models/Memory.php
+++ app/Models/Memory.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Storage;
 
 class Memory extends Model
 {
@@ -16,8 +18,24 @@ class Memory extends Model
         'is_approved' => 'boolean',
     ];
 
+    protected $appends = ['media_url'];
+
     public function wedding(): BelongsTo
     {
         return $this->belongsTo(Wedding::class);
+    }
+
+    protected function mediaUrl(): Attribute
+    {
+        return Attribute::get(function () {
+            if (! $this->media_path) {
+                return null;
+            }
+
+            /** @var \Illuminate\Filesystem\FilesystemAdapter $disk */
+            $disk = Storage::disk('s3');
+
+            return $disk->url($this->media_path);
+        });
     }
 }
