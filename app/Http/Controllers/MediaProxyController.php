c1c724666ff199b855ca701460135b035dbf8b12
Murat Adıyaman <muratadiyaman0@gmail.com>
2026-08-14 15:51:02 +0300

    added download for proxy on bootstrap

--- app/Http/Controllers/MediaProxyController.php
+++ app/Http/Controllers/MediaProxyController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Filesystem\FilesystemAdapter;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Storage;
 use Symfony\Component\HttpFoundation\StreamedResponse;
@@ -10,7 +11,7 @@ use Symfony\Component\HttpFoundation\StreamedResponse;
 class MediaProxyController extends Controller
 {
     // GET /api/media/{path}
-    public function show(string $path): StreamedResponse|Response
+    public function show(Request $request, string $path): StreamedResponse|Response
     {
         /** @var FilesystemAdapter $disk */
         $disk = Storage::disk('s3');
@@ -19,15 +20,22 @@ class MediaProxyController extends Controller
             abort(404);
         }
 
+        $headers = [
+            'Content-Type' => $disk->mimeType($path) ?? 'application/octet-stream',
+            'Cache-Control' => 'public, max-age=31536000, immutable',
+        ];
+
+        if ($request->boolean('download')) {
+            $filename = basename($path);
+            $headers['Content-Disposition'] = "attachment; filename=\"{$filename}\"";
+        }
+
         return response()->stream(function () use ($disk, $path) {
             $stream = $disk->readStream($path);
             fpassthru($stream);
             if (is_resource($stream)) {
                 fclose($stream);
             }
-        }, 200, [
-            'Content-Type' => $disk->mimeType($path) ?? 'application/octet-stream',
-            'Cache-Control' => 'public, max-age=31536000, immutable',
-        ]);
+        }, 200, $headers);
     }
 }
